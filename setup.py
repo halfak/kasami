05b030e6a1b8fbe2e9ecceab4d5f1d382319483e
halfak <aaron.halfaker@gmail.com>
2016-09-03 12:13:57 -0500

    Standardizes README, file structure, and setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'description': 'An implementation a basic PCFG parser/scorer in Python',
-    'author': 'Arthur Tilley',
-    'url': 'https://github.com/aetilley/pcfg',
-    'author_email': 'aetilley@gmail.com',
-    'version': '0.1',
-    'install requires': ['nose'],
-    'packages': ['pcfg'],
-    'scripts': [],
-    'name': 'pcfg'
-}
-
-setup(**config)
+setup(
+    description='An implementation a basic PCFG parser/scorer in Python',
+    author='Arthur Tilley',
+    url='https://github.com/aetilley/pcfg',
+    author_email='aetilley@gmail.com',
+    version='0.0.1',
+    install_requires=[],
+    packages=['pcfg'],
+    scripts=[],
+    name='pcfg'
+)
